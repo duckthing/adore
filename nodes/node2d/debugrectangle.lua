ed6d9538bb038db0b8b101b3ba1eaec9a2db7de6
duckthing <114840481+duckthing@users.noreply.github.com>
2026-07-11 15:08:40 -0500

    Improved DebugRectangle

--- nodes/node2d/debugrectangle.lua
+++ nodes/node2d/debugrectangle.lua
@@ -16,6 +16,8 @@ function DRect:new(x, y)
 
 	---@type number # How fast we should spin each update
 	self.spinSpeed = 0
+	self.width = 20
+	self.height = 35
 
 	self.albedo = {love.math.colorFromBytes(
 		love.math.random(40, 210),
@@ -24,7 +26,13 @@ function DRect:new(x, y)
 	)}
 
 	local bounds = self._localContentRect
-	bounds:iSetComponents(-10, -10, 20, 35)
+	bounds:iSetComponents(-10, -17.5, 20, 35)
+end
+
+function DRect:_onDimensionsSet()
+	local width, height = self.width, self.height
+	self._localContentRect:iSetComponents(width * -0.5, height * -0.5, width, height)
+	self:_onGlobalBoundsChanged()
 end
 
 function DRect:draw()
@@ -36,6 +44,12 @@ function DRect:update(dt)
 	if self.spinSpeed ~= 0 then
 		self:rotate(self.spinSpeed * dt)
 	end
+end
+
+function DRect._addDefinition(entry)
+	entry:newNumber("spinSpeed", 0)
+	entry:newNumber("width", 20, 0, nil, nil, "%_onDimensionsSet")
+	entry:newNumber("height", 35, 0, nil, nil, "%_onDimensionsSet")
 end
 
 return DRect
