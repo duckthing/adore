53e11dcbd55b37e2edb3e5becaba65df16c77e5a
duckthing <114840481+duckthing@users.noreply.github.com>
2026-07-21 14:12:01 -0500

    Toolbox: Enum properties use DropdownMenu

--- toolbox/ui/inspector/previewer/enumpreviewer.lua
+++ toolbox/ui/inspector/previewer/enumpreviewer.lua
@@ -5,6 +5,8 @@ local Nodes = Adore.Nodes
 local Previewer = require(ADORE_PATH..".toolbox.ui.inspector.previewer")
 
 local Button = Nodes("Button")
+---@type PopupMenu
+local PopupMenu = Nodes("PopupMenu")
 
 ---@class Previewer.Enum: Previewer
 local EnumP = Previewer:extend()
@@ -14,25 +16,43 @@ function EnumP:newValueLabel(object, property, propertyName)
 	local button = Button(tostring(val))
 		:setAnchors(1, 0, 1, 1)
 		:setOffsets(0, 0, -80, 0)
-	button.clicked:connect(self, "attemptSet")
+	button.clicked:connect(self, "showPopup")
 
 	return button
 end
 
-function EnumP:onInput()
+function EnumP:showPopup()
+	local property = self.property
+	---@cast property Property.Enum
+
+	-- Create the list of items from the valid enum map
+	local map = property:getValueMap(self.object, self.propertyName)
+	---@type PopupMenu.Item[]
+	local items = {}
+	for name, _ in pairs(map) do
+		items[#items+1] = {label = tostring(name), value = name}
+	end
+
+	local menu = PopupMenu(items)
+	menu._destroyOnClose = true
+	menu.itemSelected:connect(self, "onItemSelected")
+	menu:setAnchors(0, 1, 0, 1)
+	-- Add it to the button
+	self.children[2]:addChild(menu)
+	menu:popup()
+end
+
+function EnumP:onItemSelected(menu, itemIndex, item)
+	self:attemptSet(item.value)
+end
+
+function EnumP:onInput(item)
 	local object, property, propertyName =
 		self.object, self.property, self.propertyName
 	---@cast property Property.Enum
 
-	local currVal = property:get(object, propertyName)
-	local validValues = property:getValueMap(object, propertyName)
-	local newVal = next(validValues, currVal)
-	if newVal == nil then
-		newVal = next(validValues)
-	end
-
-	property:set(object, propertyName, newVal)
-	self.value:setText(tostring(newVal))
+	property:set(object, propertyName, item)
+	self.value:setText(tostring(item))
 end
 
 return EnumP
