84cf9acce4effa93a99c5f501e59516b4fa5f597
duckthing <114840481+duckthing@users.noreply.github.com>
2026-09-02 13:00:38 -0500

    Toolbox: Previewer.NodeRef can set to nil

--- toolbox/ui/inspector/previewer/noderefpreviewer.lua
+++ toolbox/ui/inspector/previewer/noderefpreviewer.lua
@@ -16,22 +16,37 @@ function NodeRefP:new(node, property, propertyName, inspector)
 	self.inspector = inspector
 end
 
+function NodeRefP:construct(object, property, propertyName)
+	self.nameLabel = self:newNameLabel(object, property, propertyName)
+	self.value = self:newValueLabel(object, property, propertyName)
+
+	local nilButton = Button("x")
+		:setAnchors(1, 0, 1, 1)
+		:setOffsets(-20, 0, 0, 0)
+	nilButton.clicked:connect(self, "onInput")
+
+	self:addChild(self.nameLabel)
+	self:addChild(self.value)
+	self:addChild(nilButton)
+end
+
 function NodeRefP:newValueLabel(object, property, propertyName, inspector)
 	---@type Object
 	local val = property:get(object, propertyName)
 	local selectable = val and val._adoreSelectable
 	local name = tostring(val)
-	if not selectable then name = ("%s [INTERNAL]"):format(name) end
+	if not selectable and selected ~= nil then name = ("%s [INTERNAL]"):format(name) end
 	local button = Button(name)
 		:setAnchors(1, 0, 1, 1)
-		:setOffsets(-100, 0, 0, 0)
-	button.clicked:connect(self, "onInput")
+		:setOffsets(-120, 0, -20, 0)
+	button.clicked:connect(self, "focusNode")
 	button:setDisabled(not selectable)
 
 	return button
 end
 
-function NodeRefP:onInput()
+-- Focuses on the set Node
+function NodeRefP:focusNode()
 	local object, property, propertyName =
 		self.object, self.property, self.propertyName
 	---@type boolean
@@ -39,6 +54,14 @@ function NodeRefP:onInput()
 	if val then
 		self.inspector.sceneTree:focusNode(val)
 	end
+end
+
+function NodeRefP:onInput()
+	local object, property, propertyName =
+		self.object, self.property, self.propertyName
+	property:set(object, propertyName, nil)
+	self.value:setText(property:get(object, propertyName) or "nil")
+	self.value:setDisabled(true)
 end
 
 return NodeRefP
