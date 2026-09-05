36a7d583a4390485bce7a48ad67a9cae824d2c42
duckthing <114840481+duckthing@users.noreply.github.com>
2026-09-05 00:18:31 -0500

    Toolbox: Set Previewer.NodeRef by drag/drop

--- toolbox/ui/inspector/previewer/noderefpreviewer.lua
+++ toolbox/ui/inspector/previewer/noderefpreviewer.lua
@@ -23,24 +23,53 @@ function NodeRefP:construct(object, property, propertyName)
 	local nilButton = Button("x")
 		:setAnchors(1, 0, 1, 1)
 		:setOffsets(-20, 0, 0, 0)
-	nilButton.clicked:connect(self, "onInput")
+	nilButton.clicked:connect(self, "makeNil")
 
 	self:addChild(self.nameLabel)
 	self:addChild(self.value)
 	self:addChild(nilButton)
 end
 
+---@param self Button
+local function _canDropData(self, posX, posY, data)
+	if data and type(data) == "table" and data.IS_NODE then
+		return true
+	end
+end
+
+---@param self Button
+local function _getDragData(self)
+	-- Return the contained Node
+	---@type Previewer.NodeRef
+	local previewer = self.previewer
+	return previewer.property:get(previewer.object, previewer.propertyName)
+end
+
+---@param self Button
+local function _dropData(self, posX, posY, data)
+	if data and type(data) == "table" and data.IS_NODE then
+		---@type Previewer.NodeRef
+		local previewer = self.previewer
+		previewer:attemptSet(data)
+	end
+end
+
 function NodeRefP:newValueLabel(object, property, propertyName, inspector)
 	---@type Object
 	local val = property:get(object, propertyName)
-	local selectable = val and val._adoreSelectable
 	local name = tostring(val)
-	if not selectable and selected ~= nil then name = ("%s [INTERNAL]"):format(name) end
+	local selectable = val and val._adoreSelectable
+	if not selectable and val ~= nil then name = ("%s [INTERNAL]"):format(name) end
 	local button = Button(name)
 		:setAnchors(1, 0, 1, 1)
 		:setOffsets(-120, 0, -20, 0)
 	button.clicked:connect(self, "focusNode")
 	button:setDisabled(not selectable)
+
+	button._canDropData = _canDropData
+	button._getDragData = _getDragData
+	button._dropData = _dropData
+	button.previewer = self
 
 	return button
 end
@@ -56,12 +85,22 @@ function NodeRefP:focusNode()
 	end
 end
 
-function NodeRefP:onInput()
+---Used for a Button connection; sets this property to `nil`
+function NodeRefP:makeNil()
+	return self:attemptSet()
+end
+
+function NodeRefP:onInput(node)
 	local object, property, propertyName =
 		self.object, self.property, self.propertyName
-	property:set(object, propertyName, nil)
-	self.value:setText(property:get(object, propertyName) or "nil")
-	self.value:setDisabled(true)
+	property:set(object, propertyName, node)
+
+	local val = property:get(object, propertyName)
+	local name = tostring(val)
+	local selectable = val and val._adoreSelectable
+	if not selectable and val ~= nil then name = ("%s [INTERNAL]"):format(name) end
+	self.value:setText(name)
+		:setDisabled(not selectable)
 end
 
 return NodeRefP
